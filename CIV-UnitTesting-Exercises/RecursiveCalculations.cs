78b8e5007f30e134036fbb4d0cac944e09054ad9
ubkychin <anguyen@swin.edu.au>
2019-08-12 15:50:48 +1000

    Update RecursiveCalculations.cs
    changed access of all methods to public
    
    changed Fibonacci to correct spelling

--- CIV-UnitTesting-Exercises/RecursiveCalculations.cs
+++ CIV-UnitTesting-Exercises/RecursiveCalculations.cs
@@ -8,7 +8,7 @@
         /// <param name="num2"></param>
         /// <param name="numFibs"></param>
         /// <returns></returns>
-        int FibocacciRec(int num1, int num2, int numFibs) {
+        public int FibonacciRec(int num1, int num2, int numFibs) {
             if (numFibs > 0) {
                 numFibs--;
                 num2 = num1 + num2;
@@ -23,7 +23,7 @@
         /// <param name="input"></param>
         /// <param name="ans"></param>
         /// <returns></returns>
-        int FactorialRec(int input, int ans) {
+        public int FactorialRec(int input, int ans) {
             if (input == 0) {
                 return ans;
             }
@@ -38,7 +38,7 @@
         /// </summary>
         /// <param name="num"></param>
         /// <returns></returns>
-        bool FibCheck(int num) {
+        public bool FibCheck(int num) {
             int ans = 0;
             int a = 1, b =1;
             
@@ -60,7 +60,7 @@
         /// <param name="num"></param>
         /// <param name="divisor"></param>
         /// <returns></returns>
-        bool PrimeCheckRec(int num, int divisor) {
+        public bool PrimeCheckRec(int num, int divisor) {
             if (num == divisor) {
                 return true;
             }
